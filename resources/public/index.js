7f8abf5bd6b69943d2032510aea572875e6b9e50
eric <eric.gesell@wellspringworldwide.com>
2014-07-22 11:28:36 -0500

    added ajax call to fetch data

--- resources/public/index.js
+++ resources/public/index.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 
 
-  //apply dataTable controls
+  //load json data
+  $.ajax({
+    url:"exercise.json",    
+    success: function (response) {
+
+      trainData = response.data;
+      console.log("success");
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+      console.log("error");
+      debugger;
+    }
+  });
+
+  //insert data into table
+
   $("#data").dataTable();
 });
