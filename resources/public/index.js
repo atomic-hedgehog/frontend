d42c7310b155deb47c98f451bfeae7d25de3c8e7
eric <eric.gesell@wellspringworldwide.com>
2014-07-22 13:32:24 -0500

    added filters for unique items and invalid train lines.  Filled in missing data values

--- resources/public/index.js
+++ resources/public/index.js
@@ -2,6 +2,41 @@ $(document).ready(function() {
 
   var trainData = null;
 
+  
+  var hashify = function(item){
+    var hash = "";
+    for (var key in item) {
+      if (item.hasOwnProperty(key)) {
+        hash += item[key];
+      }
+    }
+
+    return hash;
+  };
+
+  var mapProperties = function(item, f){
+    for (var key in item) {
+      if (item.hasOwnProperty(key)) {
+        item[key] = f(item[key]);
+      }
+    }
+
+    return item;
+  };
+
+  var filterTrainLines = function(data){
+    return data.filter(function(item){
+      //inArray returns -1 if the item is not present
+      return ($.inArray(item.trainLine, ["Metra", "Amtrak", "El"]) > -1);
+    });
+  };
+
+  var fixMissingData = function(trainDataItem){
+    return mapProperties(trainDataItem, function(property) {
+      return property === "" ? "unknown" : property;
+    });
+  }
+
   var trainCellTemplate = function(trainDataItemProperty) {
     return "<td>" + trainDataItemProperty + "</td>";
   };
@@ -18,15 +53,30 @@ $(document).ready(function() {
     return row + "</tr>";
   };
 
-  //load json data
+  var uniqueData = function(data) {
+
+    var uniqueData = [];
+    var uniqueItems = {};
+
+    $.each(data, function(index, item) {
+      uniqueItems[hashify(item)] = item;
+    });
+
+    for (var key in uniqueItems) {
+      if (uniqueItems.hasOwnProperty(key)) {
+        uniqueData.push(uniqueItems[key]);
+      }
+    }
+
+    return uniqueData;
+  };
+
   $.ajax({
     url:"exercise.json",    
     success: function (response) {
-
-      trainData = response.data;
-
+      trainData = uniqueData(filterTrainLines(response.data));
       $.each(trainData, function(index, item) {
-        $(".body").append(trainRowTemplate(item));
+        $(".body").append(trainRowTemplate(fixMissingData(item)));
       });
       $("#data").dataTable();
       console.log("success");
