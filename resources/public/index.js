2b74eefa73e7a66ea22265129a8ce2b3e2bba392
eric <eric.gesell@wellspringworldwide.com>
2014-07-22 11:42:44 -0500

    removed test data from html. set up quick manual templates for train data.  Applied dataTable after data is loaded

--- resources/public/index.js
+++ resources/public/index.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+  var trainData = null;
+
+  var trainCellTemplate = function(trainDataItemProperty) {
+    return "<td>" + trainDataItemProperty + "</td>";
+  };
+
+  var trainRowTemplate = function(trainDataItem) {
+    var row = "<tr>";
+
+    //since there's only 4 of these we can call manually.
+    row = row + trainCellTemplate(trainDataItem.trainLine);
+    row = row + trainCellTemplate(trainDataItem.routeName);
+    row = row + trainCellTemplate(trainDataItem.runNumber);
+    row = row + trainCellTemplate(trainDataItem.operatorId);
+
+    return row + "</tr>";
+  };
 
   //load json data
   $.ajax({
@@ -7,15 +24,15 @@ $(document).ready(function() {
     success: function (response) {
 
       trainData = response.data;
+
+      $.each(trainData, function(index, item) {
+        $(".body").append(trainRowTemplate(item));
+      });
+      $("#data").dataTable();
       console.log("success");
     },
     error: function (xhr, ajaxOptions, thrownError) {
       console.log("error");
-      debugger;
     }
   });
-
-  //insert data into table
-
-  $("#data").dataTable();
 });
